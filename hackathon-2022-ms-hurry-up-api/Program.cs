7fc77617de25397d05ad222aa7d3522cb412a509
SamuelLucas-ffcg <samuel.lucas@ffcg.se>
2022-09-09 18:17:06 +0200

    Added startup stuff

--- hackathon-2022-ms-hurry-up-api/Program.cs
+++ hackathon-2022-ms-hurry-up-api/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("corsapp");
 
 app.UseHttpsRedirection();
 
